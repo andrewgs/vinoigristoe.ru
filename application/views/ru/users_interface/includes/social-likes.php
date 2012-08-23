68e64527aa72f93b8769fb4431a3d7d18bde693b
Vladimir Kharseev <vkharseev@gmail.com>
2012-08-23 17:23:31 +0400

    23-08-2012-17-25

--- application/views/ru/users_interface/includes/social-likes.php
+++ application/views/ru/users_interface/includes/social-likes.php
@@ -1,15 +1,38 @@
 <div class="social-likes">
 	<p>Нравятся цимлянские вина?<br/>Расскажите о нем друзьям!</p>
 	<div class="like">
+		<!--<div id="vk_like"></div>-->
 		<img src="<?=$baseurl;?>images/vk.png" alt="" />
 	</div>	
 	<div class="like">
-		<img src="<?=$baseurl;?>images/gplus.png" alt="" />
-	</div><br />
-	<div class="like">
-		<img src="<?=$baseurl;?>images/tw.png" alt="" />
-	</div>
-	<div class="like">
+		<!--<div class="fb-like" data-href="http://vinoigristoe.ru" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="arial"></div>-->
 		<img src="<?=$baseurl;?>images/fb.png" alt="" />
 	</div>
+
+	<!--
+	<script type="text/javascript" src="http://userapi.com/js/api/openapi.js?52"></script>
+	<script type="text/javascript">
+		VK.init({
+			apiId : 3092259,
+			onlyWidgets : true
+		});
+	</script>
+	<script type="text/javascript">
+		VK.Widgets.Like("vk_like", {
+			type : "mini"
+		});
+	</script>
+	<div id="fb-root"></div>
+	<script>
+		( function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if(d.getElementById(id))
+				return;
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "//connect.facebook.net/ru_RU/all.js#xfbml=1&appId=118650518205495";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
+	-->
 </div>

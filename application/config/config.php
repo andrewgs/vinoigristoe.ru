943cc2d2781faebac1ea99b29fd69f36c3d8468f
Vladimir Kharseev <vkharseev@gmail.com>
2012-10-23 14:14:08 +0400

    23-10-2012-14-15

--- application/config/config.php
+++ application/config/config.php
@@ -244,7 +244,7 @@ $config['encryption_key'] = 'abe7807cbb38e0cc56de694ab41ea7c1';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'reducation';
+$config['sess_cookie_name']		= 'vinoigristoe';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= TRUE;
